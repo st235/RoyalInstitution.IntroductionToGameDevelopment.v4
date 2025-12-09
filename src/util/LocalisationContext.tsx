939d4e45f4fface4f6412c601277a37cc1293c4b
Alex Dadukin <alexanderdadukin@gmail.com>
2025-12-09 21:30:28 +0000

    Fix lint issues

--- src/util/LocalisationContext.tsx
+++ src/util/LocalisationContext.tsx
@@ -1,55 +1,58 @@
-import { useAppSelector } from '@/hooks/redux';
+import { useAppSelector } from "@/hooks/redux";
 
-import { type PropsWithChildren, createContext, useContext} from 'react';
-import { useFetch } from '@/hooks/useFetch';
+import { type PropsWithChildren, createContext, useContext} from "react";
+import { useFetch } from "@/hooks/useFetch";
 
 type TranslationsDict = {[Key: string]: {[Key: string]: string}};
 
 type LocalisationEnvironment = {
-  readonly translations: TranslationsDict | undefined,
+    readonly translations: TranslationsDict | undefined,
 }
 
 const LocalisationContext = createContext<LocalisationEnvironment | null>(null);
 
 interface LocalisationProviderProps extends PropsWithChildren {
-  translationsRes: string;
+    translationsRes: string;
 }
 
 function LocalisationProvider(props: LocalisationProviderProps) {
-  const [translationsDict, error, isLoading] = useFetch<TranslationsDict>(props.translationsRes);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [translationsDict, _error, _isLoading] = useFetch<TranslationsDict>(props.translationsRes);
 
-  return (
-    <LocalisationContext value={{ translations: translationsDict }}>
-      {props.children}
-    </LocalisationContext>
-  );
+    return (
+        <LocalisationContext value={{ translations: translationsDict }}>
+            {props.children}
+        </LocalisationContext>
+    );
 }
 
 function t<T>(key: T): T {
-  if (!key) {
-    return key;
-  }
+    if (!key) {
+        return key;
+    }
 
-  const localeState = useAppSelector(selector => selector.localeState);
-  const context = useContext(LocalisationContext);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const localeState = useAppSelector(selector => selector.localeState);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const context = useContext(LocalisationContext);
 
-  const lang = localeState.selectedLanguage ?? localeState.defaultLanguage;
-  const defaultLang = localeState.defaultLanguage;
-  const dict = context?.translations;
+    const lang = localeState.selectedLanguage ?? localeState.defaultLanguage;
+    const defaultLang = localeState.defaultLanguage;
+    const dict = context?.translations;
 
-  if (!dict) {
-    return key;
-  }
+    if (!dict) {
+        return key;
+    }
 
-  let localisedKey = key;
-  if (dict[defaultLang]) {
-    localisedKey = dict[defaultLang][key];
-  }
-  if (dict[lang] && dict[lang][key]) {
-    localisedKey = dict[lang][key];
-  }
+    let localisedKey = key;
+    if (dict[defaultLang]) {
+        localisedKey = dict[defaultLang][key];
+    }
+    if (dict[lang] && dict[lang][key]) {
+        localisedKey = dict[lang][key];
+    }
 
-  return localisedKey;
+    return localisedKey;
 }
 
 export default LocalisationProvider;
